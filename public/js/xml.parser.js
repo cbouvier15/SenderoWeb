f893c080fc0998a6fa0fc084adcaf75d7b9ec95c
Christian Bouvier <cbouvier@trea.uy>
2016-02-23 20:32:15 -0300

    XMLParser re-writting and modularization. Configuration file minified. Code Aesthetic.

--- public/js/xml.parser.js
+++ public/js/xml.parser.js
@@ -1,75 +1,148 @@
-function tagToVector3(tag){
-  var x = parseFloat(tag.getAttribute('x'));
-  var y = parseFloat(tag.getAttribute('y'));
-  var z = parseFloat(tag.getAttribute('z'));
-	return new THREE.Vector3(x,y,z);
-}
+/**
+  *
+  * XMLParser
+  * Module for load the play from a .XML file.
+  * Author: dernster, alarrosa14, kitab15.
+  */
 
-function loadPixelsFromXML(xmlPath, callBack){
+var XMLParser = function(){
 
-  var pixelsList = [];
+  // ###########################################################
+  // Private
+  // ###########################################################  
 
+  var pixelsQty = 0;
+  var meshesPath = {};
 
-  if (window.XMLHttpRequest)
-  {// code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp=new XMLHttpRequest();
-  }
-  else
-  {// code for IE6, IE5
-    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-
-  xmlhttp.open("GET",xmlPath,false);
-  xmlhttp.send();
-  var xmlDoc=xmlhttp.responseXML;
-  
-  console.log(xmlDoc);
-
-  var pixelsDoc = xmlDoc.getElementsByTagName("Pixel")
-  var pixelsQty = pixelsDoc.length
-
-  var loaderFunction = function(object){
-  	pixelsList.push(object);
-  	console.log("adding pixel...");
-  	if (pixelsList.length == pixelsQty){
-
-  		console.log("calling callback");
-
-      // order pixel objects by Id
-      pixelsList = _.sortBy(pixelsList, function(o){ return o.pixelId; });
-
-  		callBack(pixelsList);
-  	}
+  var tagToVector3 = function(tag){
+    var x = parseFloat(tag.getAttribute('x'));
+    var y = parseFloat(tag.getAttribute('y'));
+    var z = parseFloat(tag.getAttribute('z'));
+    return new THREE.Vector3(x,y,z);
   }
 
-  for (var i = 0; i < pixelsDoc.length; i++) {
-    
-    var pixel = pixelsDoc[i]
-    var ID = parseInt(pixel.getAttribute('id'))
-    var R = parseInt(pixel.getAttribute('r'))
-    var G = parseInt(pixel.getAttribute('g'))
-    var B = parseInt(pixel.getAttribute('b'))
+  // Parses Mesh tags and create a dictionary
+  // with modelName: modelPath
+  // Used to retrieve the model from backend
+  var parseMeshesTag = function(meshesTag){
+    var meshes = meshesTag[0].getElementsByTagName("Mesh");
+    _.each(meshes, function(mesh){
+      meshesPath[mesh.getAttribute('name')] = mesh.getAttribute('path');
+    });
+  };
 
-    var color = (R << 16) | (G << 8) | B;
-    
-    var renderTag = pixel.getElementsByTagName('Render')[0]
+  // Parses pixel's Id from Pixel Tag
+  var parseId = function (pixel){
+    return parseInt(pixel.getAttribute('id'));
+  };
 
-    var objectModelName = renderTag.getAttribute('mesh')
-    
-    var frontTag = renderTag.getElementsByTagName('Front')[0]
-    var upTag = renderTag.getElementsByTagName('Up')[0]
-    var positionTag = renderTag.getElementsByTagName('Position')[0]
-    
+  // Parses pixel's color from Pixel Tag
+  var parseColor = function(pixel){
+    var R = parseInt(pixel.getAttribute('r'));
+    var G = parseInt(pixel.getAttribute('g'));
+    var B = parseInt(pixel.getAttribute('b'));
+
+    return (R << 16) | (G << 8) | B;
+  };
+
+  // Parses pixel's model name from Pixel Tag
+  var parseModelName = function(pixel){
+    var renderTag = pixel.getElementsByTagName('Render')[0];
+    return renderTag.getAttribute('mesh');
+  };
+
+  // Parses pixel's space position from Pixel Tag
+  var parsePositionVectors = function(pixel){
+    var renderTag = pixel.getElementsByTagName('Render')[0];
+      
+    var frontTag = renderTag.getElementsByTagName('Front')[0];
+    var upTag = renderTag.getElementsByTagName('Up')[0];
+    var positionTag = renderTag.getElementsByTagName('Position')[0];
+
     var front = tagToVector3(frontTag);
     var up = tagToVector3(upTag);
     var position = tagToVector3(positionTag);
 
-    var print = function(vect){
-      return '(' + vect.x + ',' + vect.y + ',' + vect.z + ')';
-    }
-
-    addObject(objectModelName + '100.obj',position,up,front,color,ID,loaderFunction);
-
+    return {'front': front, 'up': up, 'position': position};
   };
 
-}
+  // ###########################################################
+  // Public Methods
+  // ###########################################################
+  
+  // Main method. Parses the play's XML.
+  var loadPixelsFromXML = function(xmlPath, callBack){
+
+    var pixelsList = [];
+
+    // Get XML file 
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    if (window.XMLHttpRequest){
+      xmlhttp=new XMLHttpRequest();
+    // code for IE6, IE5
+    }else{
+      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlhttp.open("GET",xmlPath,false);
+    xmlhttp.send();
+    var xmlDoc=xmlhttp.responseXML;
+    
+    // Parse Pixels
+    var pixelsDoc = xmlDoc.getElementsByTagName("Pixel");
+    pixelsQty = pixelsDoc.length;
+
+    // Parse Meshes and Paths
+    parseMeshesTag(xmlDoc.getElementsByTagName("Meshes"));
+
+    if (pixelsQty <= 0){
+      console.log(pixelsQty + " pixels were loaded from " + xmlPath);
+    }
+
+    var loaderFunction = function(object){
+      pixelsList.push(object);
+      
+      // When the whole list was loaded
+      // the callback is performed.
+      if (pixelsList.length == pixelsQty){
+        
+        // order pixel objects by Id
+        pixelsList = _.sortBy(pixelsList, function(o){ return o.pixelId; });
+        
+        callBack(pixelsList);
+      }
+    }
+
+    // Parse each pixel data
+    _.each(pixelsDoc, function(pixel, index){
+
+      var ID = parseId(pixel);
+      var color = parseColor(pixel);
+      var objectModelName = parseModelName(pixel);
+      var positionVectors = parsePositionVectors(pixel);
+      var front = positionVectors['front'];
+      var up = positionVectors['up'];
+      var position = positionVectors['position'];
+
+      addObject(meshesPath[objectModelName],position,up,front,color,ID,loaderFunction);
+    });
+  };
+
+  // Returns the play's pixels quantity
+  var getPixelsQty = function(){
+    return parseInt(pixelsQty);
+  };
+
+  // ###########################################################
+  // XML Parser
+  // ###########################################################
+
+  var oPublic = {
+    loadPixelsFromXML : loadPixelsFromXML,
+    getPixelsQty      : getPixelsQty,
+  };
+  return oPublic;
+
+}();
+
+
