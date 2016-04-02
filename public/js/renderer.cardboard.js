74787fae5567a3d6a5f130654446b4a468d0ab24
Christian Bouvier <cbouvier@trea.uy>
2016-04-02 20:45:09 -0300

    Fix for canvas loader

--- public/js/renderer.cardboard.js
+++ public/js/renderer.cardboard.js
@@ -143,12 +143,6 @@ var ThreeHelper = function(){
 
       };
 
-      manager.onLoad = function (){
-        if(ID==XMLParser.getPixelsQty()-1){
-          canvas_loader.hide();
-        }
-      }
-
       var loader = new THREE.OBJLoader( manager );
 
       /////////////////////////////////////
