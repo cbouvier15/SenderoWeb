075413cc919cc6153be7c682624efdc8f1d379fd
Christian Bouvier <cbouvier@trea.uy>
2016-04-02 15:01:50 -0300

    Only deploy web

--- deploy.sh
+++ deploy.sh
@@ -10,10 +10,10 @@ SENDERO_PATH="$HOME/Desktop/var/www/sendero/"
 WEB_PATH="$HOME/Desktop/var/www/web/"
 VR_PATH="$HOME/Desktop/var/www/vr/"
 
-echo "1. Deploying Streaming Server to $STREAMING_PATH ..."
-cp -r app.js package.json node_modules/ $STREAMING_PATH
-echo "1. Streaming Server deployed"
-echo
+# echo "1. Deploying Streaming Server to $STREAMING_PATH ..."
+# cp -r app.js package.json node_modules/ $STREAMING_PATH
+# echo "1. Streaming Server deployed"
+# echo
 
 echo "2. Deploying Sendero to $SENDERO_PATH ..."
 cp -r views/index.html public/styles/ public/images/ $SENDERO_PATH
@@ -28,10 +28,10 @@ cp -r public/js/OBJLoader.js public/js/interaction.js public/js/streaming.js pub
 echo "3. Web deployed"
 echo
 
-echo "4. Cardboard..."
-cp -r public/conf/ public/meshes/ $VR_PATH
-cp views/cardboard.html $VR_PATH
-mv $VR_PATH"cardboard.html" $VR_PATH"index.html"
-cp -r public/js/DeviceOrientationControls.cardboard.js public/js/StereoEffect.cardboard.js public/js/underscore-min.js public/js/OBJLoader.js public/js/socket.io.min.js public/js/OrbitControls.cardboard.js public/js/three.min.cardboard.js $VR_PATH"js"
-echo "Cardboard deployed!"
+# echo "4. Cardboard..."
+# cp -r public/conf/ public/meshes/ $VR_PATH
+# cp views/cardboard.html $VR_PATH
+# mv $VR_PATH"cardboard.html" $VR_PATH"index.html"
+# cp -r public/js/DeviceOrientationControls.cardboard.js public/js/StereoEffect.cardboard.js public/js/underscore-min.js public/js/OBJLoader.js public/js/socket.io.min.js public/js/OrbitControls.cardboard.js public/js/three.min.cardboard.js $VR_PATH"js"
+# echo "Cardboard deployed!"
 echo
